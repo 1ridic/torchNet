5dd1324189da7266d68cb3527ed7fd9703579a91
iridiumR <i@iridium.cyou>
2022-12-22 18:02:08 +0800

    feat: improved

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn	# 各种层类型的实现
 import torch.nn.functional as F	# 各中层函数的实现，与层类型对应，如：卷积函数、池化函数、归一化函数等等
 import torch.optim as optim	# 实现各种优化算法的包
 from torchvision import datasets, transforms
-
+from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder  # 图形数据集
 import torch.optim as optim  # pytorch优化工具箱
 import os  # os文件系统工具包
@@ -19,12 +19,50 @@ class Net(nn.Module):
         return logits
 
 class Runtime:
-    def __init__(self,l):
+    def __init__(self,layer):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Using {self.device} device")
-        self.model = Net(l).to(self.device)
-        print(self.model)
+
+        self.net = Net(layer).to(self.device)
+        print(self.net)
+
     def loadTrainData(self, path, transformer):
-        train_data=ImageFolder(path, transform=transformer)
-        print(train_data)
-        print(train_data.class_to_idx)
+        self.train_data=ImageFolder(path, transform=transformer)
+        print(self.train_data)
+        print(self.train_data.class_to_idx)
+
+    def train(self,epoch):
+        self.net.train()
+        for epoch in range(epoch):
+            running_loss = 0.0
+            running_acc = 0.0
+            for step,(features, targets) in enumerate(DataLoader(dataset=self.train_data, batch_size=1, shuffle=True),0):
+                    features = features.to(self.device)
+                    targets = targets.to(self.device)
+                    # 梯度清零，也就是把loss关于weight的导数变成0.
+                    # 进⾏下⼀次batch梯度计算的时候，前⼀个batch的梯度计算结果，没有保留的必要了。所以在下⼀次梯度更新的时候，先使⽤optimizer.zero_grad把梯度信息设置为0。
+                    self.optimizer.zero_grad()
+                    # 获取网络输出
+                    output = self.net(features)
+                    # 获取损失
+                    loss = self.loss_fn(output, targets)
+                    # 反向传播
+                    loss.backward()
+                    # 训练
+                    self.optimizer.step()
+
+                    running_loss += loss.item()
+ 
+            print('[%04d] loss: %03d' %
+                      (epoch + 1,running_loss))
+            # zero the loss
+            running_loss = 0.0
+
+        # # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
+        # accuracy = self.testAccuracy()
+        
+        # # we want to save the model if the accuracy is the best
+        # if accuracy > best_accuracy:
+        #     saveModel()
+        #     best_accuracy = accuracy
+            
--- train.py
+++ train.py
@@ -2,28 +2,51 @@ import cv2
 import os
 from model import *
 
-layer=nn.Sequential( # 构造网络
-            nn.Flatten(),
-            nn.Conv2d(64*64*3, 256, 5),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 128, 5),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 64, 5),
-            nn.MaxPool2d(2),
-            nn.Linear(32, 16),
-            nn.Linear(16, 4)
-        )
+# 图像预处理
 img_transformer = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize(75),#重设大小
-    transforms.RandomResizedCrop(64,scale=(0.8,1.0)),#随机裁剪 0.8-1.0倍
+    # transforms.ToPILImage(),
+    transforms.Resize(128),#重设大小
+    transforms.RandomResizedCrop(112,scale=(0.8,1.0)),#随机裁剪 0.8-1.0倍
     # transforms.RandomHorizontalFlip(),#随机水平翻转
     transforms.ToTensor(),#转为张量
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])#标准化
 ])
-r=Runtime(layer)
 
+# 构造网络
+layer=nn.Sequential( 
+    nn.Conv2d(
+        in_channels=3,
+        out_channels=16,
+        kernel_size=3,
+        stride=1,
+        padding=1
+        ),                               #维度变换(3,112,112) --> (16,112,112)
+    nn.BatchNorm2d(num_features=16),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=2),         #维度变换(16,112,112) --> (32,112,112)
+    nn.Conv2d(
+        in_channels=16,
+        out_channels=32,
+        kernel_size=3,
+        stride=1,
+        padding=1
+            ),                           #维度变换(32,112,112) --> (32,56,56)
+    nn.BatchNorm2d(num_features=32),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=2),        #维度变换(32,56,56) --> (32,28,28)
+    nn.Flatten(),                       #维度变换(32,28,28) --> (32*28*28,1)
+    nn.Linear(32*28*28, 10),
+    nn.Linear(10, 4)
+)
+
+
+r=Runtime(layer)
+# 优化器
+r.optimizer = optim.SGD(r.net.parameters(), lr=0.01)
+# 损失函数
+r.loss_fn = nn.CrossEntropyLoss()
+# 加载数据
 r.loadTrainData('./train/', img_transformer)
-##numpy.ndarray
-# img = cv2.imread(img_path)  # 读取图像
-# img1 = train_transformer(img)
+# 训练
+epoch=1000
+r.train(epoch)
